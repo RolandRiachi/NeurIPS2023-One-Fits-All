3dc26aa9623222eacaad130eafc9eb838923ff19
RolandRiachi <roland@riachi.com>
2023-11-30 14:14:32 -0500

    Updated training scripts for Frontier

--- Long-term_Forecasting/scripts/weather.sh
+++ Long-term_Forecasting/scripts/weather.sh
@@ -1,15 +1,32 @@
-export CUDA_VISIBLE_DEVICES=0
+#!/bin/bash
+#SBATCH -A csc538
+#SBATCH -J Weather
+#SBATCH -o ../../job_logs/ts-transfer/%x-%j.out
+#SBATCH -t 00:30:00
+#SBATCH -p batch
+#SBATCH -N 1
+
+export PATH="/ccs/home/rolandriachi/ts-transfer/ofa-env/bin:$PATH"
+export SCRATCH="/lustre/orion/csc538/scratch/rolandriachi"
+export PROJ_SHARE="/lustre/orion/csc538/proj-shared/llm2ts/"
+module load cray-python/3.9 rocm/5.6.0
+source ~/ts-transfer/ofa-env/bin/activate
+
+model_name=GPT4TS
+root_path=$SCRATCH
+
+# export CUDA_VISIBLE_DEVICES=0
 
 seq_len=512
 model=GPT4TS
 
 for percent in 100
 do
-for pred_len in 96 192 336 720
+for pred_len in 96 # 192 336 720
 do
 
 python main.py \
-    --root_path ./datasets/weather/ \
+    --root_path $PROJ_SHARE/weather/ \
     --data_path weather.csv \
     --model_id weather_$model'_'$gpt_layer'_'$seq_len'_'$pred_len'_'$percent \
     --data custom \
